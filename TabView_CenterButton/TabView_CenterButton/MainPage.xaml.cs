0f7d7434448aeea322aa38b40e3c40008867a5b4
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-27 17:40:38 +0530

    Center button sample has been added

---
+++ TabView_CenterButton/TabView_CenterButton/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.XForms.TabView;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabView_CenterButton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Message", "CenterButton Clicked", "Ok");
+        }
+
+        private void TabView_CenterButtonTapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Message", "CenterButton Clicked", "Ok");
+        }
+    }
+
+}
